c0ddf087ae0e8731365de473d06f764887e26488
rabiulislamrizon <contact.rijonislam@gmail.com>
2025-04-05 21:28:44 +0600

    first commit

---
+++ src/Home.js
@@ -0,0 +1,220 @@
+import React from "react";
+
+
+
+
+
+
+
+
+
+
+
+const Home = () => {
+
+  return (
+    <div>
+
+
+<div>
+        {/* start: Back To Top */}
+        <div className="progress-wrap" id="scrollUp">
+          <svg className="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+          </svg>
+        </div>
+        {/* end: Back To Top */}
+        {/* HEADER START */}
+        <header className="tj-header-area header-absolute">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 d-flex flex-wrap align-items-center">
+                <div className="logo-box">
+                  <a href="/">
+                    <img src="https://i.ibb.co/HY0hrrp/R.png" alt />
+                  </a>
+                </div>
+                <div className="header-info-list d-none d-md-inline-block">
+                  <ul className="ul-reset">
+                    <li><a href="mailto:contact.rijonislam@gmail.com">contact.rijonislam@gmail.com
+                    </a></li>
+                  </ul>
+                </div>
+                <div className="header-menu" id="headerMenu">
+                  <nav>
+                    <ul>
+                      <li className=" current-menu-ancestor">
+                        <a className="current-menu-item" href="/">Home</a>
+                      </li>
+                      <li><a href="#about-sec">About</a></li>
+                      <li><a href="#services-sec">Services</a></li>
+
+                      <li><a href="#skill-sec">My Skill</a></li>
+                      <li><a href="#contact-sec">Contact</a></li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="mobile-menu d-lg-none" />
+                <div className="header-button">
+                  <a href="https://www.fiverr.com/miss_reshma" class="btn tj-btn-primary" target="_blank">Hire me!</a>
+                </div>
+                <div className="menu-bar d-lg-none">
+                  <button>
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <header className="tj-header-area header-2 header-sticky sticky-out">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 d-flex flex-wrap align-items-center">
+                <div className="logo-box">
+                  <a href="/">
+                    <img src="https://i.ibb.co/HY0hrrp/R.png" alt />
+                  </a>
+                </div>
+                <div className="header-info-list d-none d-md-inline-block">
+                  <ul className="ul-reset">
+                    <li><a href="mailto:contact.rijonislam@gmail.com">contact.rijonislam@gmail.com</a></li>
+                  </ul>
+                </div>
+                <div className="header-menu">
+                  <nav>
+                    <ul>
+                      <li className=" current-menu-ancestor">
+                        <a className="current-menu-item" href="/">Home</a>
+
+                      </li>
+                      <li><a href="#about-sec">About</a></li>
+                      <li><a href="#services-sec">Services</a></li>
+
+                      <li><a href="#skill-sec">My Skill</a></li>
+                      <li><a href="#contact-sec">Contact</a></li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="mobile-menu d-lg-none" />
+                <div className="header-button">
+                  <a
+                    href="https://www.fiverr.com/rabiulrizon"
+                    className="btn tj-btn-primary blink"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Hire me!
+                  </a>
+                </div>
+                <div className="menu-bar d-lg-none">
+                  <button>
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    
+      
+
+    <section className="hero-section d-flex align-items-center" id="intro">
+        <div className="intro_text">
+          <svg viewBox="0 0 1320 300">
+            <text x="50%" y="50%" textAnchor="middle">HI</text>
+          </svg>
+        </div>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <div className="hero-content-box">
+                <span className="hero-sub-title">I am RiZon</span>
+                <h1 className="hero-title">Web Developer &<br />SEO Expert</h1>
+                <div className="hero-image-box d-md-none text-center">
+                  <img src="assets/img/hero/me.png" alt />
+                </div>
+                <p className="lead">
+                  Create a website with us for any business or SEO services. I specialize in SEO and web development tailored to your needs, including business, blog, news, eCommerce, agency, LMS, real estate, and portfolio websites. As a React and WordPress expert, MERN Stack developer, and SEO service provider, I deliver exceptional results.
+                </p>
+                <div className="button-box d-flex flex-wrap align-items-center">
+                  <a href="https://robiulislamrijon360.blogspot.com/2024/07/rabiul-islam-rizon-cv.html" className="btn tj-btn-secondary">Download CV <i className="flaticon-download" /></a>
+                  <ul className="ul-reset social-icons">
+                    <li>
+                      <a href="https://www.facebook.com/rabiulislamrizon" target="_blank" rel="noopener noreferrer">
+                        <i className="fa-brands fa-facebook" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.behance.net/me_rizon" target="_blank" rel="noopener noreferrer">
+                        <i className="fa-brands fa-behance" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/in/rabiul-islam-rizon-a92943281/" target="_blank" rel="noopener noreferrer">
+                        <i className="fa-brands fa-linkedin-in" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/rabiulislamrizon" target="_blank" rel="noopener noreferrer">
+                        <i className="fa-brands fa-github" />
+                      </a>
+                    </li>
+                  </ul>
+
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 d-none d-md-block">
+              <div className=" text-center">
+                <img src="https://i.ibb.co/9vrGKt2/profile-pic-10.png" alt />
+              </div>
+            </div>
+          </div>
+          <div className="funfact-area">
+            <div className="row">
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number"><span className="odometer" data-count={5}>0</span></div>
+                  <div className="text">Years of <br />Experience</div>
+                </div>
+              </div>
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number"><span className="odometer" >1.2</span>+</div>
+                  <div className="text">Project <br />Completed</div>
+                </div>
+              </div>
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number"><span className="odometer">1.1</span>k</div>
+                  <div className="text">Happy <br />Clients</div>
+                </div>
+              </div>
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number"><span className="odometer" >20</span></div>
+                  <div className="text">Winning of <br />Award</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+    </div>
+  );
+};
+
+export default Home;
